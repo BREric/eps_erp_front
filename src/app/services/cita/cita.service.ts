46e3da5d9209ecb48eb5f673cf9c80a9be658d0c
EricBR <eric.bedoyar@uqvirtual.edu.co>
2023-11-19 15:40:23 -0500

    nuevos cambios

--- src/app/services/cita/cita.service.ts
+++ src/app/services/cita/cita.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Cita } from 'src/app/model/cita';
 
 @Injectable({
@@ -7,11 +8,29 @@ import { Cita } from 'src/app/model/cita';
 })
 export class CitaService {
 
-  private url: string = 'http://localhost:8080/cita';
+  private baseUrl = 'http://localhost:8081/cita/registrarCita';
 
-  constructor(private http: HttpClient) { }
+  private baseMedicoUrl = 'http://localhost:8081/cita';
 
-  listar(){
-    this.http.get<Cita[]>(this.url);
+  constructor(private httpClient: HttpClient) { }
+
+  registrarCita(cita: any): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+
+    let body = new HttpParams();
+    body = body.set('especializacion', cita.especializacion);
+    body = body.set('fecha_cita', cita.fecha_cita);
+    body = body.set('motivo', cita.motivo);
+
+    return this.httpClient.post<any>(this.baseUrl, body, { headers: headers });
+  }
+
+  getCitasFuturasMedico(idMedico: number): Observable<Cita[]> {
+    const url = `${this.baseMedicoUrl}/${idMedico}/citas-futuras`;
+    return this.httpClient.get<Cita[]>(url);
+  }
+
+  listar() {
+    return this.httpClient.get<Cita[]>(this.baseUrl);
   }
 }
--- src/app/views/medico/citas-medico/citas-medico.component.ts
+++ src/app/views/medico/citas-medico/citas-medico.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+// citas-medico.component.ts
+import { Component, OnInit } from '@angular/core';
+import { CitaService } from 'src/app/services/cita/cita.service';
+import { Cita } from 'src/app/model/cita';
 @Component({
   selector: 'app-citas-medico',
   templateUrl: './citas-medico.component.html',
   styleUrls: ['./citas-medico.component.scss']
 })
-export class CitasMedicoComponent {
+export class CitasMedicoComponent implements OnInit {
+  citasFuturas: Cita[] = [];
 
+  constructor(private citaService: CitaService) { }
+
+  ngOnInit(): void {
+    // Supongamos que el ID del médico es 1 (puedes cambiarlo según tus necesidades)
+    const idMedico = 1;
+
+    this.citaService.getCitasFuturasMedico(idMedico).subscribe(
+      (data: Cita[]) => {
+        console.log(data);
+        this.citasFuturas = data;
+      },
+      (error: any) => {
+        console.error('Error al obtener citas futuras:', error);
+      }
+    );
+  }
 }
